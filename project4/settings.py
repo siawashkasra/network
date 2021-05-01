1ab2c6c9bb64eb7a9eaafbc0239ae8908d4cd74e
siawashkasra <siawashkasra@gmail.com>
2021-05-01 09:25:30 +0430

    Refactor migrations and improved pagination

--- project4/settings.py
+++ project4/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'network',
-        'USER': 'siawashkasra',
+        'USER': 'kasra',
         'PASSWORD': 'kasra@321',
         'HOST': '127.0.0.1',
         'PORT': '5432',
